acd1ac855cb9433f805a3bfc14f1623bbe1431f0
ilyay95 <43435369+ilyay95@users.noreply.github.com>
2020-03-21 21:58:00 +0300

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(obj) {
+  if (!obj) {
+    return 'Unable to determine the time of year!'
+  };
+  if (!obj.toTimeString()){
+    throw new Error();
+  }
+ let month = obj.getMonth();
+ 
+ if (month === 0 || month === 1 || month === 11) {
+   return 'winter'
+ }
+  if (month === 2 || month === 3 || month === 4) {
+   return 'spring'
+ }
+  if (month === 5 || month === 6 || month === 7) {
+   return 'summer'
+ }
+  if (month === 8 || month === 9 || month === 10) {
+   return 'autumn'
+ }
+ 
 };
