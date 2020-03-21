e6b20862c511431e42ab5570c536bc4474274c0b
ilyay95 <43435369+ilyay95@users.noreply.github.com>
2020-03-21 21:59:57 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+	if(Array.isArray(members)) {
+		return members.map(e => { if(typeof(e) == "string") return e.trim().toUpperCase()[0];}).sort().join("");
+	} else {
+		return false;
+	}
+  
 };
